fd102d617b7b8505b2edfd3aa1021ff38cf5eae8
Magichat <neil.1475369@gmail.com>
2019-01-17 20:44:43 +0000

    basic setup down

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,29 @@ var sass = require('gulp-sass');
  
 sass.compiler = require('node-sass');
  
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   return gulp.src('dev/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('prod/css'));
 });
+
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('html', () => {
+  return gulp.src('dev/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+    .pipe(gulp.dest('prod'));
+});
+
+var imagemin = require('gulp-imagemin');
  
-gulp.task('sass:watch', function () {
+gulp.task('imgmin', () =>
+    gulp.src('dev/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('prod/img'))
+);
+ 
+gulp.task('watch', function () {
+  gulp.watch('dev/*.html', gulp.series('html'));
   gulp.watch('dev/scss/', gulp.series('sass'));
 });
